3fee1dd75907dfe5a92d88b5910108c5b0ba5cba
Joao Dias <joaovbmdias@icloud.com>
2020-08-06 16:42:49 +0100

    Edit for testing automation

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup, find_packages
 from codecs import open
 
-REPO_URL = "https://github.com/joaovbmdias/brainless"
-VERSION = "0.1"
-
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
+# with open("requirements.txt") as f:
+#     required = f.read().splitlines()
+required = ['click==7.1.1', 'connexion==2.7.0', 'continuous-threading==1.1.1', 'Flask==1.1.2', 'flask-marshmallow==0.12.0', 'Flask-SQLAlchemy==2.4.1', 'marshmallow==3.6.0', 'marshmallow-sqlalchemy==0.23.0', 'pyicloud==0.9.7', 'requests==2.23.0', 'SQLAlchemy==1.3.17', 'swagger-ui-bundle==0.0.6', 'voluptuous==0.11.7', 'voluptuous-serialize==2.3.0', 'tox==3.18.1']
 
 with open("README.rst", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="brainless",
-    version=VERSION,
+    version="0.1",
     license="MIT",
-    url=REPO_URL,
+    url="https://github.com/joaovbmdias/brainless",
     description="An automatic task scheduler for Todoist",
     long_description=long_description,
     maintainer="joaovbmdias",
